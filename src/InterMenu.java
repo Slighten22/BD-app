712e8979eef1a3cb3260e6a0363a4549e15d8394
pkryszto <pp.krysztofik@gmail.com>
2018-05-05 15:05:18 +0200

    Pawel

---
+++ src/InterMenu.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class InterMenu extends JFrame {
+	public InterMenu() {
+		super("Baza danych");
+		setSize(800, 600);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
